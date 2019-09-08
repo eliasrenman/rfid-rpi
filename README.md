1d9e553d434b101915a07425d898dfabe3127e53
Abborren <elias.renman00@gmail.com>
2019-09-08 14:51:57 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# RFID PI
+#### WARNING This repository is W.I.P and all features of the program does not work proceed at your own risk!
+
+Physical check in and out system written to work with [Yogster's](https://github.com/Yogsther) rest api and web interface
+
+[Link to Rest api and web interface](https://github.com/Yogsther/te4-time)
+
+## Hardware needed
+- Raspberry pi 3 or newer.
+- RC522 RFID module
+- 4 Leds
+- 4 Resistors
+## Software needed
+- Python 3.7 or newer
+- Python3 pip
+- SSH (Optional but recommended)
+- git
+- SPI needs to be enabled ([guide](https://learn.sparkfun.com/tutorials/raspberry-pi-spi-and-i2c-tutorial/all#spi-on-pi))
+## Python dependencies
+- [SimpleMFRC522](https://github.com/pimylifeup/MFRC522-python)
+- Spidev
+- RPi.GPIO
+- [dotenv](https://github.com/theskumar/python-dotenv)
+# Installation
+Before using this program make sure that the GPIO pins are correctly wired as incorrectly wired components *CAN* 
+brick your Raspberry or components so proceed at your own risk.
+
+## Wiring
+Turn off your Rpi and connect the cables according to the picture and schematics.
+####Warning! These schematics currently don't include wiring for the LEDS
+
+![visual image](https://raw.githubusercontent.com/Abborren/rfid-rpi/master/images/visual.png)
+
+![schematics](https://raw.githubusercontent.com/Abborren/rfid-rpi/master/images/schematics.png)
+
+
+#### Remember to enable SPI and double check the wiring! 
+### Software preparation
+install the python dependencies by writing ``pip3 install `` and the package name.
+### Software cloning and installation
+After correctly doing the previous steps you can now start with the software by cloning and opening the directory.
+
+`git clone https://github.com/Abborren/rfid-rpi`
+
+`cd dir rfid-rpi`
+
+After entering the directory copy the .env.example to .env.
+
+`cp .env.example .env`
+
+Edit the .env file and add the needed values.
+```dotenv
+# HTTP REQUEST VARIABLES
+TOKEN=              # A token value that the rest api uses to validate the http request
+HOST_ENDPOINT=      # URL + /api/check if the running rest api is Yogster's
+PORT=443            # Should preferably be 443 for https
+# GENERAL ENV
+DEBUG_INPUT=true    # This is used when debugging the program without a RPI or rfid reader
+DEBUG_OUTPUT=true   # This is used to console log output instead of blinking leds
+CONSOLE_PRINT=true  # This enables or disables console printing
+```
+
+If all the wiring is correct, spi is enabled and all the python dependencies are installed the program should be ready to run.
+Run the program by writing:
+``
+sudo python3 run.py
+``
---
+++ run.py
@@ -0,0 +1,200 @@
+#  Written by Elias Renman, 2019.
+
+# !/usr/bin/env python
+import os
+from time import time, sleep
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Reader:
+
+    def __init__(self):
+        self.last_card_id = 0
+        self.last_interaction_timestamp = int(round(time()))
+        if not Util.debug_input():
+            import RPi.GPIO as GPIO
+            self.GPIO = GPIO
+
+    def loop(self):
+        req = HttpRequest()
+        try:
+            while True:
+                card_id = self.read()
+                Util.print("ID: %s\n" % card_id)
+                if self.debounce(card_id):
+                    req.handle_req(card_id)
+        except KeyboardInterrupt:
+            if not Util.debug_input():
+                self.GPIO.cleanup()
+
+            raise
+
+    def read(self):
+        from mfrc522 import SimpleMFRC522
+        reader = SimpleMFRC522()
+        Util.print("Hold a tag near the reader")
+        return reader.read_id()
+
+    def debounce(self, card_id):
+        if card_id != self.last_card_id:
+            self.update_last(card_id)
+            return True
+        else:
+            if self.last_interaction_timestamp + 10 > int(round(time())):
+                self.update_last(card_id)
+                return True
+            else:
+                return False
+
+    def update_last(self, card_id):
+        self.last_card_id = card_id
+        self.last_interaction_timestamp = int(round(time()))
+
+
+class DebugReader(Reader):
+
+    def read(self):
+        input('write anything to run:')
+        return DebugReader.get_random_cid()
+
+    @staticmethod
+    def get_random_cid():
+        import random
+        return random.randint(1, 100000000)
+
+
+class HttpRequest:
+
+    def __init__(self):
+        self.controller = (GPIOController(), DebugGPIOController())[Util.debug_output()]
+
+    def handle_req(self, card_id):
+        self.controller.process()
+        response = HttpRequest.send_request(card_id)
+        self.handle_response(response)
+
+    @staticmethod
+    def send_request(card_id):
+        import requests
+        url = os.getenv("HOST_ENDPOINT")
+        current_time = int(round(time() * 1000))
+        payload = "token=" + os.getenv("TOKEN") + "&" \
+            "card=" + str(card_id) + "&" \
+            "timestamp=" + str(current_time)
+
+        headers = {
+            'Content-Type': "application/x-www-form-urlencoded",
+            'User-Agent': "PostmanRuntime/7.16.3",
+            'Accept': "*/*",
+            'Cache-Control': "no-cache",
+            'Postman-Token': "5c7f1b44-0fa9-4619-b653-ab27c0f16761,1a3976a8-fe2b-4561-97ef-a3abd381d4d5",
+            'Accept-Encoding': "gzip, deflate",
+            'Connection': "keep-alive",
+            'cache-control': "no-cache"
+        }
+        response = requests.request("POST", url, data=payload, headers=headers)
+        text = response.text
+        response.close()
+        return text
+
+    def handle_response(self, response):
+        import json
+        Util.print(response)
+        response_json = json.loads(response)
+
+        if response_json["success"]:
+            if response_json["check_in"]:
+                self.controller.check_in()
+            else:
+                self.controller.check_out()
+        else:
+            self.controller.error()
+        self.controller.process()
+
+
+class GPIOController:
+
+    def __init__(self):
+        if os.getenv("DEBUG_OUTPUT") != "true":
+            import RPi.GPIO as GPIO
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setwarnings(False)
+            GPIO.setup(18, GPIO.OUT)
+            self.GPIO = GPIO
+        self.processing = False
+
+    def process(self):
+        if self.processing:
+            self.processing = False
+            self.GPIO.output(1, self.GPIO.LOW)
+        else:
+            self.processing = True
+            self.GPIO.output(1, self.GPIO.HIGH)
+
+    def check_in(self):
+        self.blink_output(0, 2, 0.3)
+
+    def check_out(self):
+        self.blink_output(0, 2, 0.3)
+
+    def error(self):
+        self.blink_output(0, 2, 0.3)
+
+    def blink_output(self, pin, amount, timeout):
+
+        for i in range(amount):
+            self.GPIO.output(pin, self.GPIO.HIGH)
+            sleep(timeout)
+            self.GPIO.output(pin, self.GPIO.LOW)
+            sleep(timeout)
+
+
+class DebugGPIOController(GPIOController):
+
+    def process(self):
+        if self.processing:
+            self.processing = False
+            Util.print("Proccesing LED off")
+        else:
+            self.processing = True
+            Util.print("Proccesing LED on")
+
+    def check_in(self):
+        Util.print("check in triggered")
+        super().check_in()
+
+    def check_out(self):
+        Util.print("check out triggered")
+        super().check_out()
+
+    def error(self):
+        Util.print("error triggered")
+        super().error()
+
+    def blink_output(self, pin, amount, timeout):
+        for i in range(amount):
+            Util.print("blinking on" + pin)
+            sleep(timeout)
+            Util.print("blinking off" + pin)
+            sleep(timeout)
+
+
+class Util:
+    @staticmethod
+    def debug_output():
+        return os.getenv("DEBUG_OUTPUT") == "true"
+
+    @staticmethod
+    def debug_input():
+        return os.getenv("DEBUG_INPUT") == "true"
+    
+    @staticmethod
+    def print(_str):
+        if os.getenv("CONSOLE_PRINT") == "true":
+            print(_str)
+
+
+reader = (Reader(), DebugReader())[Util.debug_input()]
+reader.loop()
